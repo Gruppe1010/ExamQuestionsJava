109b530144928309320a941f6d012c2db9aa8b64
Vibs <vibej@hotmail.com>
2021-06-18 10:39:39 +0200

    DROP syntax indsat

--- src/com/company/Opgaver/SQL01.java
+++ src/com/company/Opgaver/SQL01.java
@@ -44,6 +44,7 @@ S8. Write a line of sql code that creates a new database.
 -------------------------------------------------
 S9. Write a line of sql code that removes the above database.
 ! Syntax for DROP: DROP OBJECT_TYPE [ IF EXISTS ] OBJECT_NAME
+// https://sqlhints.com/tag/drop-table-if-exists/
 
 
 + DROP DATABASE IF EXIST newdb;
