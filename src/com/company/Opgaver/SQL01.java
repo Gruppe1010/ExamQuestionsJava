66196e1835fe502476a67053b7ba95f272cd5bb8
RRoager <rasmus-roager@hotmail.com>
2021-06-18 09:51:32 +0200

    SQL

--- src/com/company/Opgaver/SQL01.java
+++ src/com/company/Opgaver/SQL01.java
@@ -16,6 +16,7 @@ S3. What are the 2 major types of commands in sql ?
 + INSERT INTO
 -------------------------------------------------
 S4. What sql could be used to copy all data from one table into another table.
+
 + INSERT INTO table2
 + SELECT * FROM table1
 -------------------------------------------------
@@ -26,26 +27,32 @@ S5. You have 2 tables.
 
 create a table that defines which students are related to which classes.
 
-
++ Vi opretter en many-to-many tabel hvor id_class og id_student er foreign keys der referere til hver sin table i den nye
 -------------------------------------------------
 S6. Inspired by S5, create a sql statement that shows all students in a certain class.
 
++ SELECT * FROM classes JOIN student_classes ON id_student WHERE classes = "Programmering";
 -------------------------------------------------
 S7. Write one line of sql code that adds a varchar column to a table.
     and the column should not allow Null values, and default value should be 'x'
 
++ ALTER TABLE student ADD addcolumn VARCHAR(255) NOT NULL DEFAULT "x";
 -------------------------------------------------
 S8. Write a line of sql code that creates a new database.
 
++ CREATE DATABASE newdb;
 -------------------------------------------------
 S9. Write a line of sql code that removes the above database.
 
++ DROP IF EXIST DATABASE newdb;
 -------------------------------------------------
 S10. Write the shortest possible line of sql that creates a new table.
 
++ CREATE TABLE newtable(preben VARCHAR(255));
 -------------------------------------------------
 S11. Write the shortest possible line of sql that creates a new table with a primary key.
 
++ CREATE TABLE newtable1(bent INT NOT NULL, PRIMARY KEY(`bent`));
 -------------------------------------------------
 S12. Write sql that removes the above table.
 
